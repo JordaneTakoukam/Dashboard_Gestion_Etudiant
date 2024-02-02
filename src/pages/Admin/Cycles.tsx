9508ed884ecff9c7fec67afe9ca8a30ddd5ce7bc
arnold kezeter <arnoldkezeter@gamil.com>
2024-02-02 08:03:48 +0100

    implementation de l'interface cycle (reste formulaire, pagination)

--- src/pages/Admin/Cycles.tsx
+++ src/pages/Admin/Cycles.tsx
@@ -1,13 +1,29 @@
 import Breadcrumb from "../../components/Breadcrumb";
+import Table from "../../components/Tables/TableCycle/Table";
 
+export interface Cycle{
+    code:string;
+    libelle:string;
+}
 
 const Cycles = () => {
     return (
         <>
             <Breadcrumb pageName="Cycles" />
+            <Table data={listCycle}/>
 
         </>
     );
 };
 
 export default Cycles;
+export const listCycle: Cycle[] = [
+    {
+        code:"CA",
+        libelle:"Cycle A",
+    },
+    {
+        code:"CB",
+        libelle:"Cycle B",
+    },
+];

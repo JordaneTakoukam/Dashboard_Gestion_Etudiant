d56caf5cb935f6978b0a09bec038ba495125bf7b
arnold kezeter <arnoldkezeter@gamil.com>
2024-02-02 08:11:14 +0100

    implementation de l'interface niveau (reste formulaire, pagination)

--- src/pages/Admin/Niveaux.tsx
+++ src/pages/Admin/Niveaux.tsx
@@ -1,13 +1,29 @@
 import Breadcrumb from "../../components/Breadcrumb";
+import Table from "../../components/Tables/TableNiveau/Table";
 
+export interface Niveau{
+    code:string;
+    libelle:string;
+}
 
 const Niveaux = () => {
     return (
         <>
             <Breadcrumb pageName="Niveaux" />
+            <Table data={listNiveau}/>
 
         </>
     );
 };
 
 export default Niveaux;
+export const listNiveau: Niveau[] = [
+    {
+        code:"N1",
+        libelle:"1ère année",
+    },
+    {
+        code:"N2",
+        libelle:"2ème année",
+    },
+];
